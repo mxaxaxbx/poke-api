0ba1fed453c07cabc3e1e6f9037955c8e3e25312
Miguel Arenas <miguel.arenas@sitidoctor.com>
2021-08-02 12:05:58 -0500

    set image to poke

--- assests/js/ramapi.js
+++ assests/js/ramapi.js
@@ -24,12 +24,13 @@ const llenarDatos = ( data =[] ) => {
     let html = '';
 
     data.forEach( (pj) => {
+        callImage(pj);
         html += `<div class="col mt-5">`;
         html += `<div class="card" style="width: 10rem;">`;
         // html += `<img src="${pj.image}" class="card-img-top" alt="${pj.name}"/>`;
         html += `<div class="card-body">`;
-        html += `<h5 class="card-title">${pj.name}</h5>`;
-        // html += `<p class="card-text">Estado: ${pj.status}</p>`;
+        html += `<h5 class="card-title">Nombre: ${pj.name}</h5>`;
+        html += `<p class="card-text" id="pj-${pj.name}"></p>`;
         // html += `<p class="card-text">Especie: ${pj.species}</p>`;
         html += `</div>`;
         html += `</div>`;
@@ -64,4 +65,18 @@ const getResults = ( op = 'more') => {
     if( op === 'more') offset = offset + 20;
     else offset = offset - 20;
     getData(limit, offset);
+}
+
+const callImage = (pj) => {
+    fetch(pj.url)
+        .then( (response) => response.json() )
+        .then( (json) => {
+            let html = "";
+            html += `<img src="${json.sprites.back_default}" alt="${pj.name}"/>`;
+            document.getElementById(`pj-${pj.name}`).innerHTML = html;
+
+        })
+        .catch((err) => {
+            console.log('error: ', err);
+        });
 }
